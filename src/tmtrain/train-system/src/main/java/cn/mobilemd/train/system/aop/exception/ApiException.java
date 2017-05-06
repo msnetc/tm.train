13716e6bf8d256b1275ba6973f93dcdd6ad38a09
jerry <jerrymiao@renqidai.cc>
2017-05-06 20:16:55 +0800

    加入了aop即对所有异常进行了捕捉并返回数据

---
+++ src/tmtrain/train-system/src/main/java/cn/mobilemd/train/system/aop/exception/ApiException.java
@@ -0,0 +1,9 @@
+package cn.mobilemd.train.system.aop;
+
+public class ApiException extends Exception {
+
+    public ApiException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/tmtrain/train-system/src/main/java/cn/mobilemd/train/system/api/HelloController.java
@@ -0,0 +1,27 @@
+package cn.mobilemd.train.system.api;
+
+import cn.mobilemd.train.system.aop.ApiException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() throws Exception {
+        throw new Exception("发生错误");
+    }
+
+    @RequestMapping("/json")
+    public String json() throws ApiException {
+        throw new ApiException("发生错误2");
+    }
+
+    @RequestMapping("/")
+    public String index(ModelMap map) {
+        map.addAttribute("host", "http://blog.didispace.com");
+        return "index";
+    }
+
+}
